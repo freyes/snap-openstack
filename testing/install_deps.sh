3a9c75fff59f485465e8d6025562d2d759e83437
Felipe Reyes <felipe.reyes@canonical.com>
2025-10-17 12:10:16 -0300

    testing: Install terraform
    Signed-off-by: Felipe Reyes <felipe.reyes@canonical.com>

--- testing/install_deps.sh
+++ testing/install_deps.sh
@@ -5,6 +5,7 @@ if [ "x$(which terragrunt)" != "x0" ]; then
     sudo chmod +x /usr/local/bin/terragrunt
 fi
 
+# install opentofu
 if [ "x$(which tofu)" != "x0" ]; then
     sudo apt-get update
     sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
@@ -20,6 +21,19 @@ deb-src [signed-by=/etc/apt/keyrings/opentofu.gpg,/etc/apt/keyrings/opentofu-rep
     sudo chmod a+r /etc/apt/sources.list.d/opentofu.list
     sudo apt-get update
     sudo apt-get install -y -qq tofu
+fi
+
+# install terraform
+if [ "x$(which tofu)" != "x0" ]; then
+    sudo apt-get update -q
+    sudo apt-get install -yq gnupg software-properties-common
+    wget -O- https://apt.releases.hashicorp.com/gpg | \
+        gpg --dearmor | \
+        sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | \
+        sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt-get update -q
+    sudo apt-get install -yq terraform
 fi
 
 if [ "x$(which virsh)" != "x0" ]; then
